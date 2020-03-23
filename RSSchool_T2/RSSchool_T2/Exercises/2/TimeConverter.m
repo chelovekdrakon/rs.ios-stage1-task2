96cbc416554890dea6297a5729fe4095bfdbeace
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-23 21:56:00 +0300

    feat: Time Converter (Execise 2)

--- RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
+++ RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
@@ -3,6 +3,36 @@
 @implementation TimeConverter
 // Complete the following function
 - (NSString*)convertFromHours:(NSString*)hours minutes:(NSString*)minutes {
-    return @"";
+    NSInteger hoursInt = [hours intValue];
+    NSInteger minutesInt = [minutes intValue];
+    
+    if (minutesInt > 60 || minutesInt < 0 || hoursInt < 0 || hoursInt > 24) {
+        return @"";
+    }
+    
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    numberFormatter.numberStyle = NSNumberFormatterSpellOutStyle;
+    
+    NSString *result = @"";
+    NSMutableString *minutesStr = [[numberFormatter stringFromNumber:@(minutesInt > 30 ? 60 - minutesInt : minutesInt)] mutableCopy];
+    NSMutableString *hoursStr = [[numberFormatter stringFromNumber:@((minutesInt > 30) ? hoursInt + 1 : hoursInt)] mutableCopy];
+    NSString *pastTo = (minutesInt < 30) ? @"past" : @"to";
+    
+    [hoursStr replaceOccurrencesOfString:@"-" withString:@" " options:kNilOptions range:NSMakeRange(0, hoursStr.length)];
+    [minutesStr replaceOccurrencesOfString:@"-" withString:@" " options:kNilOptions range:NSMakeRange(0, minutesStr.length)];
+    
+    if (minutesInt == 0) {
+        result = [NSString stringWithFormat:@"%@ o' clock", hoursStr];
+    } else if (minutesInt == 30) {
+        result = [NSString stringWithFormat:@"half past %@", hoursStr];
+    } else if (minutesInt % 15 == 0) {
+        result = [NSString stringWithFormat:@"quarter %@ %@", pastTo, hoursStr];
+    } else if (minutesInt == 1 || minutesInt == 59) {
+        result = [NSString stringWithFormat:@"1 minute %@ %@", pastTo, hoursStr];
+    } else {
+        result =  [NSString stringWithFormat:@"%@ minutes %@ %@", minutesStr, pastTo, hoursStr];
+    }
+    
+    return result;
 }
 @end
