3c7ce76037c6bee453c60c416bbe1272cc04662a
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-23 23:02:15 +0300

    feat: AbbreviationMaker (Exercise 3)

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,46 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    NSMutableString *mutableA = [a mutableCopy];
+    NSCharacterSet *lowercaseLetters = [NSCharacterSet lowercaseLetterCharacterSet];
+    
+    for (int i = 0; i < b.length; i++) {
+        NSString *charB = [b substringWithRange:NSMakeRange(i, 1)];
+        
+        while(true) {
+            NSString *charA = [mutableA substringWithRange:NSMakeRange(i, 1)];
+            
+            if ([charB isEqualToString:charA]) {
+                break;
+            }
+            
+            NSRange range = [charA rangeOfCharacterFromSet:lowercaseLetters];
+            BOOL isLowerCased = range.location != NSNotFound;
+            
+            if (isLowerCased) {
+                NSString *charA_uppercased = [charA uppercaseString];
+                
+                if ([charB isEqualToString:charA_uppercased]) {
+                    [mutableA replaceCharactersInRange:NSMakeRange(i, 1) withString:charA_uppercased];
+                    
+                    if (mutableA.length == i + 1) {
+                        return [mutableA isEqual:b] ? @"YES" : @"NO";
+                    }
+                    
+                    break;
+                } else {
+                    [mutableA deleteCharactersInRange:NSMakeRange(i, 1)];
+                    
+                    if (mutableA.length == i) {
+                        return [mutableA isEqual:b] ? @"YES" : @"NO";
+                    }
+                }
+            } else {
+                return @"NO";
+            }
+        }
+    }
+    
+    return [mutableA containsString:b] ? @"YES" : @"NO";
 }
 @end
