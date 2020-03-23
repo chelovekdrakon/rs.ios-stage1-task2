2a92849b3cc9691224644c2ddb87a0ccd4b255ef
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-23 20:51:58 +0300

    feature: Date Formatter (Exercise 1)

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,22 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    NSString *dateString = [NSString stringWithFormat:@"%@ %@ %@", day, month, year];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"dd MM yyyy";
+    dateFormatter.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
+    
+    NSDate *date = [dateFormatter dateFromString:dateString];
+    
+    if (date) {
+        dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier: @"ru"];
+        dateFormatter.dateFormat = @"dd MMMM, EEEE";
+        
+        return [dateFormatter stringFromDate:date];
+    } else {
+        return @"Такого дня не существует";
+    }
 }
 
 @end
