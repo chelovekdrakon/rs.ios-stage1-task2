decd63e921b0cc11efeb211ff54f43a55897149b
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-23 23:30:04 +0300

    feat: productFibonacciSequenceFor (Exercise 6)

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,19 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    int prev = 0;
+    int curr = 1;
+    int prod = [number intValue];
+    int multiplied = prev * curr;
+
+    while (multiplied < prod) {
+        int temp = curr;
+        curr += prev;
+        prev = temp;
+        multiplied = prev * curr;
+    }
+    
+    return @[@(prev), @(curr), @(multiplied == prod ? 1 : 0)];
 }
+
 @end
